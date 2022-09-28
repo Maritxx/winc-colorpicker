f6d43f0be28f5d31208244a3c7ab366a142261af
Maritxx <maritdevries1@live.nl>
2022-09-28 17:07:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,4 @@
-/*REQUIREMENTS
-1. Hamburger menu that will open when hovered over the icon. The menu will close when it is not being hovered.
-    -CAN ALSO BE CLICK TO OPEN (basic requirement)
-2. Hamburger menu has several menu-items. Clicking on one of these menu-items changes the background of the body to the color that menu-item represents.
-3. The menu-items have the background-color they represent (each menu-item has a different background-color).
-    -CAN ALSO JUST BE TEXT THAT HAS THE COLOR-NAME IN IT (basic requirement)
-4. The user can only click on one menu-item from the menu. When clicking on a menu-item, the hamburger menu closes.
-5. The transition for the menu has to be sliding in and out (smoothly).
-    -CAN ALSO BE IGNORED (basic requirement)
-6. There is text on the webpage that describes the current color, it will change depending on which color is selected.
-    -CAN ALSO BE IGNORED (basic requirement)
-7. The menu-items are radio buttons so that you can see which one is 'active' at that moment. 
-    -CAN ALSO BE IGNORED (basic requirement)
-8. You are able to change the colors of the web-page with your keyboard (1 for first-color, 2 for second-color etc.)
-    -CAN ALSO BE IGNORED (basic requirement) */
-
-
-/*TO DO
-1. Border around active button in menu. */
-
-
-//TOGGLES MENU
+//FUNCTION TO TOGGLE MENU
 const menuToggle = document.querySelector("#menuToggle");
 const menuContent = document.querySelector("#menuContent");
 
@@ -30,16 +9,22 @@ const toggleMenu = function() {
 menuToggle.addEventListener("click", toggleMenu);
 
 
-//FUNCTION TO CHANGE COLOR OF BG + CHANGE TEXT BASED ON SELECTED ELEMENT.
+//FUNCTION TO CHANGE COLOR OF BG, ADD BORDER TO SELECTED ITEM IN MENU & CHANGE TEXT BASED ON SELECTED ELEMENT.
 const selectColor = function(selectedElement) {
     const selectedColor = window.getComputedStyle(selectedElement).getPropertyValue('background-color');
     document.body.style.backgroundColor = selectedColor;
 
     document.getElementById("currentColor").innerText = "The current color is " + selectedElement.innerHTML;
+
+    Array.from(colorOptions).forEach((option) => {
+        option.classList.remove("activeBorder");
+    });
+
+    selectedElement.classList.add("activeBorder");
 }
 
 
-//EVENT LISTENERS THAT TARGET THE CORRECT ELEMENT.
+//EVENT LISTENERS THAT TARGET THE CORRECT ELEMENT ON CLICK OR KEYPRESS.
 const colorOptions = document.querySelectorAll("#menuContent li");
 
 for (let i = 0; i < colorOptions.length; i++) {
@@ -63,6 +48,3 @@ document.addEventListener("keyup", function(e) {
         };
     };
 });
-
-
-
